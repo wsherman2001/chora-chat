5ffa9e031c21ecc09de4f9ae36f37543aa363d2b
wsherman2001 <w.sherman2001@gmail.com>
2025-07-16 11:54:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export default function App() {
-  return <h1 style={{ color: 'white', backgroundColor: '#121212', padding: '2rem' }}>Welcome to Chora Chat!</h1>
+function App() {
+  return (
+    <h1>Welcome to Chora Chat!</h1>
+  );
 }
+
+export default App;
