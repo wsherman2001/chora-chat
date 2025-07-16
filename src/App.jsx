cca9dbb48ceed92f6de3c9334041c16ef623a326
wsherman2001 <w.sherman2001@gmail.com>
2025-07-16 21:50:41 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 // src/App.jsx
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Layout";
+import Chat from "./Chat";
 
-function App() {
-  return <Layout />;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/channel/general" />} />
+          <Route path="/channel/:channelId" element={<Chat />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
-
-export default App;
