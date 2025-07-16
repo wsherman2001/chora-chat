64d7646d9ab5dcf22d776b4cba4dae82ea9cfffe
wsherman2001 <w.sherman2001@gmail.com>
2025-07-16 11:20:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: ".", // kořen je aktuální složka (tam musí být index.html)
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
 })
